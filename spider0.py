4d282f413f59a064e3f77f290cd6f420836f43d3
jiehunt <jiehunt@github.com>
2018-03-22 20:12:04 +0900

    add get_price_list func

--- spider0.py
+++ spider0.py
@@ -32,6 +32,83 @@ class newhighlist:
         self.code_name = []
         self.stockinfo_list = []
 
+def get_price_list(urlbase):
+    try:
+        http = urllib3.PoolManager()
+        request = http.request('GET', urlbase)
+        # response = http.urlopen(request)
+        # html = response.read()
+    except urllib.URLError as e:
+        if hasattr(e, "code"):
+            print('error code')
+            print (e.code)
+        if hasattr(e, "reason"):
+            print('error reason')
+            print (e.reason)
+
+
+    soup = BeautifulSoup(request.data, "lxml")
+    #prices = soup.find_all("dd",class_="stc-now")
+    # prices = soup.find_all("td", class_="stoksPrice")
+    prices = soup.find_all("td")
+
+    Sts = []
+
+    for p in prices:
+        nStr = ""
+        if len(p.contents)  == 1:
+            pp = (p.contents[0])
+        else:
+          continue
+        pp = nStr.join(pp)
+        Sts.append(pp)
+
+    # for s in Sts:
+    #     print (s)
+    #     print ("--------")
+
+    fmtcnt = -1
+    mysheet = pd.DataFrame(columns=['Date','StartPrice','HighPrice','LowPrice','EndPrice','Count','FinalPrice'])
+    mycell = pd.Series(index=['Date','StartPrice','HighPrice','LowPrice','EndPrice','Count','FinalPrice'])
+    col = 0
+
+    # StartPrice HighPrice LowPrice EndPrice Count FinalPrice
+    for s in Sts:
+        # if (str(s).find('年') and str(s).find('月') ):
+        if s.endswith(r'日'):
+            # mysheet=mysheet.append({"Date": s}, ignore_index=True)
+            mycell['Date'] = s
+            fmtcnt = 0
+            continue
+        if fmtcnt == 0:
+            #mysheet["StartPrice"] = s
+            mycell['StartPrice'] = s
+            fmtcnt = 1
+            continue
+        if fmtcnt == 1:
+            mycell['HighPrice'] = s
+            fmtcnt = 2
+            continue
+        if fmtcnt == 2:
+            mycell['LowPrice'] = s
+            fmtcnt = 3
+            continue
+        if fmtcnt == 3:
+            mycell['EndPrice'] = s
+            fmtcnt = 4
+            continue
+        if fmtcnt == 4:
+            mycell['Count'] = s
+            fmtcnt = 5
+            continue
+        if fmtcnt == 5:
+            mycell['FinalPrice'] = s
+            fmtcnt = 6
+            mysheet=mysheet.append(mycell,ignore_index=True)
+            continue
+
+    return mysheet
+
 
 
 """
@@ -53,82 +130,21 @@ for item in newhigh:
     # getpriceandjikasougaku(url,item)
     cnt += 1
 
-urlbase = 'https://stocks.finance.yahoo.co.jp/stocks/history/?code=6083.T'
-try:
-    http = urllib3.PoolManager()
-    request = http.request('GET', urlbase)
-    # response = http.urlopen(request)
-    # html = response.read()
-except urllib.URLError as e:
-    if hasattr(e, "code"):
-        print('error code')
-        print (e.code)
-    if hasattr(e, "reason"):
-        print('error reason')
-        print (e.reason)
+mysheet = pd.DataFrame()
+for n in range(1,4):
+    # urlbase = 'https://stocks.finance.yahoo.co.jp/stocks/history/?code=6083.T'
+    urlbase = 'https://info.finance.yahoo.co.jp/history/?code=6083.T'
+    urlbase = urlbase + '&p=' + str(n)
+    newsheet = get_price_list(urlbase)
+    if n > 0:
+      mysheet = pd.concat((mysheet, newsheet))
+    else:
+      mysheet = newsheet
 
-
-soup = BeautifulSoup(request.data, "lxml")
-#prices = soup.find_all("dd",class_="stc-now")
-# prices = soup.find_all("td", class_="stoksPrice")
-prices = soup.find_all("td")
-# print (prices)
-
-Sts = []
-
-for p in prices:
-    nStr = ""
-    pp = (p.contents[0])
-    pp = nStr.join(pp)
-    Sts.append(pp)
-  
-for s in Sts:
-    print (s)
-    print ("--------")
-
-fmtcnt = -1
-mysheet = pd.DataFrame(columns=['Date','StartPrice','HighPrice','LowPrice','EndPrice','Count','FinalPrice'])
-mycell = pd.Series(index=['Date','StartPrice','HighPrice','LowPrice','EndPrice','Count','FinalPrice'])
-col = 0
-
-# StartPrice HighPrice LowPrice EndPrice Count FinalPrice
-for s in Sts:
-    # if (str(s).find('年') and str(s).find('月') ):
-    if s.endswith(r'日'):
-        # mysheet=mysheet.append({"Date": s}, ignore_index=True)
-        mycell['Date'] = s
-        fmtcnt = 0
-        continue
-    if fmtcnt == 0:
-        #mysheet["StartPrice"] = s
-        mycell['StartPrice'] = s
-        fmtcnt = 1
-        continue
-    if fmtcnt == 1:
-        mycell['HighPrice'] = s
-        fmtcnt = 2
-        continue
-    if fmtcnt == 2:
-        mycell['LowPrice'] = s
-        fmtcnt = 3
-        continue
-    if fmtcnt == 3:
-        mycell['EndPrice'] = s
-        fmtcnt = 4
-        continue
-    if fmtcnt == 4:
-        mycell['Count'] = s
-        fmtcnt = 5
-        continue
-    if fmtcnt == 5:
-        mycell['FinalPrice'] = s
-        fmtcnt = 6
-        mysheet=mysheet.append(mycell,ignore_index=True)
-        continue
+mysheet = mysheet.reset_index(drop=True)
 
 print (mysheet)
 print ( "*************")
-print (mycell)
 
 
 """
